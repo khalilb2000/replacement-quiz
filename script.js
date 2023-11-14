01fd12f502eca3585c7bf89fe6c0cb4ff095b8e1
khalilb2000 <khalilb2000@gmail.com>
2023-11-13 19:45:36 -0600

    still trying to fix the script.js

--- script.js
+++ script.js
@@ -10,7 +10,16 @@ document.addEventListener("DOMContentLoaded", function() {
   const startButton = document.getElementById("start-button");
 
   function loadQuestion() {
-      // ... existing loadQuestion function ...
+      const question = questions[currentQuestion];
+      questionText.textContent = question.question;
+      optionsContainer.innerHTML = '';
+
+      question.options.forEach((option, index) => {
+          const optionElement = document.createElement("div");
+          optionElement.textContent = option;
+          optionElement.addEventListener("click", () => checkAnswer(index));
+          optionsContainer.appendChild(optionElement);
+      });
 
       if (currentQuestion === 0) {
           startTimer();
@@ -29,7 +38,47 @@ document.addEventListener("DOMContentLoaded", function() {
       }, 1000);
   }
 
-  // ... existing checkAnswer, showResult, and other functions ...
+  function checkAnswer(selectedIndex) {
+      const question = questions[currentQuestion];
+      if (question.options[selectedIndex] === question.answer) {
+          score++;
+      }
+
+      currentQuestion++;
+
+      if (currentQuestion < questions.length) {
+          loadQuestion();
+      } else {
+          showResult();
+      }
+  }
+
+  function showResult() {
+      questionText.textContent = "Quiz finished!";
+      optionsContainer.innerHTML = '';
+      nextButton.style.display = 'none';
+      resultsContainer.textContent = `Your score: ${score} out of ${questions.length}`;
+
+      // Prompt the user for initials
+      const userInitials = prompt("Enter your initials:");
+
+      // Save the score and initials to local storage
+      const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+      highScores.push({ initials: userInitials, score: score });
+      localStorage.setItem("highScores", JSON.stringify(highScores));
+
+      // Display high scores or perform other actions as needed
+      displayHighScores();
+  }
+
+  function displayHighScores() {
+      // Retrieve high scores from local storage and display them
+      const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+      resultsContainer.textContent += "\nHigh Scores:\n";
+      highScores.forEach((entry) => {
+          resultsContainer.textContent += `${entry.initials}: ${entry.score}\n`;
+      });
+  }
 
   startButton.addEventListener("click", loadQuestion);
   nextButton.addEventListener("click", () => {
