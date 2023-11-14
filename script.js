e2b58158d4dd7b498355c7ee2d250a9a65fbd356
khalilb2000 <khalilb2000@gmail.com>
2023-11-13 19:41:51 -0600

    fixing quiz to implement timer as well as fix css and other minor tweaks

--- script.js
+++ script.js
@@ -1,54 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let currentQuestion = 0;
-    let score = 0;
-  
-    const questionText = document.getElementById("question-text");
-    const optionsContainer = document.getElementById("options-container");
-    const resultsContainer = document.getElementById("results-container");
-    const nextButton = document.getElementById("next-button"); // Define nextButton inside the event listener
-  
-    function loadQuestion() {
-      const question = questions[currentQuestion];
-      questionText.textContent = question.question;
-      optionsContainer.innerHTML = '';
-  
-      question.options.forEach((option, index) => {
-        const optionElement = document.createElement("div");
-        optionElement.textContent = option;
-        optionElement.addEventListener("click", () => checkAnswer(index));
-        optionsContainer.appendChild(optionElement);
-      });
-    }
-  
-    function checkAnswer(selectedIndex) {
-      const question = questions[currentQuestion];
-      if (question.options[selectedIndex] === question.answer) {
-        score++;
+  let currentQuestion = 0;
+  let score = 0;
+  let timeRemaining = 60; // Set your desired quiz time in seconds
+
+  const questionText = document.getElementById("question-text");
+  const optionsContainer = document.getElementById("options-container");
+  const resultsContainer = document.getElementById("results-container");
+  const nextButton = document.getElementById("next-button");
+  const startButton = document.getElementById("start-button");
+
+  function loadQuestion() {
+      // ... existing loadQuestion function ...
+
+      if (currentQuestion === 0) {
+          startTimer();
       }
-  
-      currentQuestion++;
-  
+  }
+
+  function startTimer() {
+      const timerInterval = setInterval(function () {
+          timeRemaining--;
+          resultsContainer.textContent = `Time Remaining: ${timeRemaining}s`;
+
+          if (timeRemaining <= 0) {
+              clearInterval(timerInterval);
+              showResult();
+          }
+      }, 1000);
+  }
+
+  // ... existing checkAnswer, showResult, and other functions ...
+
+  startButton.addEventListener("click", loadQuestion);
+  nextButton.addEventListener("click", () => {
       if (currentQuestion < questions.length) {
-        loadQuestion();
-      } else {
-        showResult();
+          loadQuestion();
       }
-    }
-  
-    function showResult() {
-      questionText.textContent = "Quiz finished!";
-      optionsContainer.innerHTML = '';
-      nextButton.style.display = 'none';
-      resultsContainer.textContent = `Your score: ${score} out of ${questions.length}`;
-    }
-  
-    nextButton.addEventListener("click", () => {
-      if (currentQuestion < questions.length) {
-        loadQuestion();
-      }
-    });
-  
-    // Start the quiz by loading the first question
-    loadQuestion();
   });
-  
+
+  loadQuestion();
+});
